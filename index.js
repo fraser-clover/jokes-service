3f673dca41bfe20c4e4743df3ee160685f431de3
Fraser Clover <leesel.fraser@clover.com>
2023-02-16 10:55:55 -0500

    finished

--- index.js
+++ index.js
@@ -1,49 +1,75 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
-const { Op } = require('sequelize');
+const { Joke } = require("./db");
+const { Op } = require("sequelize");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
 
     const { tags, content } = req.query;
-    
-    const where = {};
-   
-    // if(tags) where.tags = tags;
-    // if(content) where.joke = content;
 
-    const jokes = await Joke.findAll({
+    // const where = {};
+
+    // if (tags) where.tags = tags;
+    // if (content) where.joke = content;
+    
+    let jokes;
+
+    if(tags || content){
+       jokes = await Joke.findAll({
+    
         where: {
-          tags: {
-            [Op.like]: `%${tags}%`
-          }
+          [Op.or]: [
+            {
+              tags: {
+                [Op.like]: `%${tags}%`
+              }
+            },
+            { 
+              joke: {
+                [Op.like]: `%${content}%`
+              }
+            },
+          ],
         },
-        where: {
-          joke: {
-            [Op.like]: `%${content}%`
-          }
-        }
-    });
+  
+      });
+    } else {
 
-    
+       jokes = await Joke.findAll({
+      });
 
+    }
+   
 
-  //   where[key] =  {
-  //     [Op.like]: `%${req.query[key]}%` // search within the string, not only exact matches
-  // };
-    
+    // const jokesByTag = await Joke.findAll({
+    //   where: {
+    //     tags: {
+    //       [Op.like]: `%${tags}%`
+    //     }
+    //   }
+    // });
+
+    // const jokesByContent = await Joke.findAll({
+    //   where: {
+    //     joke: {
+    //       [Op.like]: `%${content}%`
+    //     }
+    //   }
+    // });
+
+    //   where[key] =  {
+    //     [Op.like]: `%${req.query[key]}%` // search within the string, not only exact matches
+    // };
 
     res.send(jokes);
-
-
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
